7ca0867e9fed01769743e2599f1cf166580aaf9e
ayaalbakri <aya.albakri3@gmail.com>
2017-09-18 20:18:37 -0700

    serverImage

--- node-post-reqs/server.js
+++ node-post-reqs/server.js
@@ -1,22 +1,26 @@
 //Fill in vars here
-var 
-= /* Missing require statment */
-var 
-= /* Missing require statment */
-var 
-= /* Missing require statment */
+var http= require('http'); 
+var data= [{username:'aya'}]
+var fs= require('fs');
 
 http.createServer(function (request, response) {
  var path = url.parse(request.url, true).pathname;
 
  if (request.method === 'POST') {
    /*========YOUR CODE HERE=========*/
+   request.on('data' , function (chunk) {
+   	/* body... */
+   	data += chunk;
+   	data.push(JSON.parse(data));
+    response.end();
+
+   })
  } else if (request.method === 'GET') {
    if(path === '/'){
      response.writeHead(200,{'Content-Type':'text/html'});
      fs.readFile(__dirname + '/index.html', function(err, data){
        if(err) console.log(err);
-       response.end(data);
+       response.end(JSON.stringify(data) );
      });
    }
  } else {
